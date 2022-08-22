087f5753c9c1561c69d747dbfe72882f9c10ef99
Saeedovic <Seedxlss@gmail.com>
2022-08-23 00:03:01 +0400

    Goal Oriented Action Planning

---
+++ Assets/AgentVisualiser/GAgentVisual.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class GAgentVisual : MonoBehaviour
+{
+    public GAgent thisAgent;
+
+    
+    void Start()
+    {
+        thisAgent = this.GetComponent<GAgent>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GOAP/GWorld.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GWorld {
+
+    
+    private static readonly GWorld instance = new GWorld();
+    
+    private static WorldStates world;
+  
+    private static Queue<GameObject> patients;
+    
+    private static Queue<GameObject> cubicles;
+
+    static GWorld() {
+
+        
+        world = new WorldStates();
+        
+        patients = new Queue<GameObject>();
+       
+        cubicles = new Queue<GameObject>();
+        
+        GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cubicle");
+       
+        foreach (GameObject c in cubes) {
+
+            cubicles.Enqueue(c);
+        }
+
+        
+        if (cubes.Length > 0) {
+            world.ModifyState("FreeCubicle", cubes.Length);
+        }
+
+       
+        Time.timeScale = 5.0f;
+    }
+
+    private GWorld() {
+
+    }
+
+   
+    public void AddPatient(GameObject p) {
+
+        // Add the patient to the patients Queue
+        patients.Enqueue(p);
+    }
+
+    // Remove patient
+    public GameObject RemovePatient() {
+
+        if (patients.Count == 0) return null;
+        return patients.Dequeue();
+    }
+
+   
+    public void AddCubicle(GameObject p) {
+
+        // Add the patient to the patients Queue
+        cubicles.Enqueue(p);
+    }
+
+   
+    public GameObject RemoveCubicle() {
+
+        // Check we have something to remove
+        if (cubicles.Count == 0) return null;
+        return cubicles.Dequeue();
+    }
+
+    public static GWorld Instance {
+
+        get { return instance; }
+    }
+
+    public WorldStates GetWorld() {
+
+        return world;
+    }
+}
